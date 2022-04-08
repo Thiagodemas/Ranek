725a3f547032edf784830ea95f4ee34cd7cad932
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-04-08 15:44:11 -0300

    Cria components e ajuda o HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    
-  </div>
+  <section>
+    <h1>👍️ Compre ou venda 👎️</h1>
+    <ProductsSearch/>
+    <ProductsList/>
+  </section>
 </template>
 
 <script>
+import ProductsSearch from "@/components/ProductsSearch.vue";
+import ProductsList from "@/components/ProductsList.vue";
 
 export default {
   name: 'homeView',
+  components: {
+    ProductsSearch,
+    ProductsList
+  }
 }
 </script>
+
+<style scoped>
+  h1 {
+    color: #87f;
+    margin-top: 40px;
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
