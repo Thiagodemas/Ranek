1e5b2a5c39226d233bb9dbdfee45ad7ec07e8132
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-04-08 16:31:18 -0300

    Cria arquivo services e configura o axios

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-        
-    </div>
+    <section class="container-products">
+        <div v-for="product in products" :key="product.id">
+           <img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0].titulo">
+            <p class="price">{{product.preco}}</p>
+            <h2 class="title">{{product.nome}}</h2>
+            <p>{{product.descricao}}</p>
+        </div>
+    </section>
 </template>
 
 <script>
+import { api } from '@/services.js'
+
 export default {
-    name: "ProductsList"
+    name: "ProductsList",
+    data(){
+        return {
+            products: null
+        }
+    },
+    methods: {
+        getProducts() {
+            api.get("/produto")
+            .then(response => {
+                this.products = response.data;
+            });
+            
+        }
+    },
+    created() {
+        this.getProducts();
+    }
 }
 </script>
 
