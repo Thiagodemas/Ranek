484b1b2f65d736526bec2e107cc014c33a76e0a1
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-04-09 10:25:49 -0300

    Ajusta componente de busca

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <input name="search" id="search" type="text" v-model="search">
+        <input name="search" id="search" placeholder="Buscar..." type="text" v-model="search">
         <input type="submit" id="lupa" value="search" @click.prevent="productSearch">
     </form>
 </template>
@@ -21,9 +21,37 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     form{
         margin: 2rem auto;
         text-align: center;
+        max-width: 600px;
+        margin: 30px auto 60px auto;
+        position: relative;
+    }
+
+    #search {
+        width: 100%;
+        box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+        padding: 20px;
+        border: none;
+    }
+
+    #search:focus,
+    #search:hover {
+        transform: scale(1.1);
+    }
+
+    #lupa {
+        width: 62px;
+        height: 62px;
+        background: url("../assets/search.svg") no-repeat center center;
+        text-indent: -150px;
+        border: none;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        box-shadow: none;
     }
 </style>
